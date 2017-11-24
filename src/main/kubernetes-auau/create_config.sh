ad4c609a49c1650c8262b25fd842b9a766ec4fe3
John Erik Halse <johnerikhalse@gmail.com>
2017-11-24 17:14:34 +0100

    Removed namespaces Added Persistent Disc Claims +++

--- src/main/kubernetes-auau/create_config.sh
+++ src/main/kubernetes-auau/create_config.sh
@@ -7,4 +7,10 @@ if [ -z "$EXTERNAL_HOSTNAME" -o -z "$PILT_ADDR" ]; then
   exit
 fi
 
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR
+
 envsubst '${EXTERNAL_HOSTNAME} ${PILT_ADDR}' < configmap.template > configmap.yml
+
+kubectl apply -f configmap.yml
+kubectl apply -f auau.yml

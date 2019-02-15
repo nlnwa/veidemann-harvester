79805c42138721336eeb630c693121092460cad9
johnerikhalse <johnerikhalse@gmail.com>
2019-02-15 22:00:54 +0100

    Moved controller to separate repo

--- veidemann-controller/src/main/distfiles/bin/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Eventually import extra trusted CA certificates
-if [ -e /veidemann/trustedca/tls.crt ]; then
-    openssl x509 -in /veidemann/trustedca/tls.crt -inform pem -out /veidemann/config/ca.der -outform der
-    keytool -importcert -trustcacerts -noprompt -alias startssl -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -file /veidemann/config/ca.der
-fi
-
-exec /veidemann/bin/veidemann-controller

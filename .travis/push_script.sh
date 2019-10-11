f70bb7558c73c6eb1ddecdfc43d1bcc958874a2e
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2019-10-11 12:52:30 +0200

    Simplify deploy

--- .travis/push_script.sh
+++ .travis/push_script.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-DOCKER_TAG=latest
+DOCKER_TAG=${1:-latest}
 
-if [[ -n "$TRAVIS_TAG" || "$TRAVIS_BRANCH" == "master" && "$TRAVIS_EVENT_TYPE" == "push" ]]; then
-  if [[ -n "${TRAVIS_TAG}" ]]; then
-    DOCKER_TAG=${TRAVIS_TAG}
-  fi
-
-  mvn -B -Pdocker-build-and-push -Ddocker.tag="$DOCKER_TAG" install;
-fi
+mvn -B -Pdocker-build-and-push -Ddocker.tag="${DOCKER_TAG}" install;
